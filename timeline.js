5826b3c806c431bc4b0717a9852dc3d3b516117f
Nikhil Anand <mail@nikhil.io>
2018-07-11 19:19:05 -0500

    Smooooooth scroll ahead and behind

--- timeline.js
+++ timeline.js
@@ -1,3 +1,31 @@
+const scrollDuration = 900;
+
+Math.easeInOutQuad = function (current_time, start_val, value_change, duration) {
+    current_time /= duration/2;
+    if (current_time < 1) return value_change/2*current_time*current_time + start_val;
+    current_time--;
+    return -value_change/2 * (current_time*(current_time-2) - 1) + start_val;
+};
+
+function smoothScroll(element, to, duration) {
+    let start = element.scrollLeft;
+    let change = to - start;
+    let currentTime = 0;
+    let increment = 20;
+
+    let animateScroll = () => {        
+        currentTime += increment;
+        var val = Math.easeInOutQuad(currentTime, start, change, duration);
+        element.scrollLeft = val;
+
+        if(currentTime < duration) {
+            setTimeout(animateScroll, increment);
+        }
+    }
+
+    animateScroll();
+}
+
 const nodesWithData = document.querySelectorAll('.node-with-data');
 
 function hideAllPopups() {
@@ -26,9 +54,9 @@ nodesWithData.forEach(node => {
 
 // Scroll timeline to left and right
 document.getElementById('ahead').addEventListener('click', e => {
-    document.getElementById('timeline-wrapper').scrollLeft = 0;
+    smoothScroll(document.getElementById('timeline-wrapper'), 0, scrollDuration);
 });
 
 document.getElementById('behind').addEventListener('click', e => {
-    document.getElementById('timeline-wrapper').scrollLeft = document.getElementById('timeline').getBoundingClientRect().width;
+    smoothScroll(document.getElementById('timeline-wrapper'), document.getElementById('timeline').getBoundingClientRect().width, scrollDuration);
 });
