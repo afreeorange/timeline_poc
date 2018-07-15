191488b5fde187892136f4b5fb3124b47515e68f
Nikhil Anand <mail@nikhil.io>
2018-07-15 11:19:46 -0500

    Bleh

--- timeline.js
+++ timeline.js
@@ -1,7 +1,15 @@
-const scrollDuration = 500;
+/**
+ * @license MIT, http://github.com/asvd/dragscroll
+ */
+!function(e,n){"function"==typeof define&&define.amd?define(["exports"],n):"undefined"!=typeof exports?n(exports):n(e.dragscroll={})}(this,function(e){var n,t,o=window,l=document,c="mousemove",r="mouseup",i="mousedown",m="EventListener",d="add"+m,s="remove"+m,f=[],u=function(e,m){for(e=0;e<f.length;)(m=(m=f[e++]).container||m)[s](i,m.md,0),o[s](r,m.mu,0),o[s](c,m.mm,0);for(f=[].slice.call(l.getElementsByClassName("dragscroll")),e=0;e<f.length;)!function(e,m,s,f,u,a){(a=e.container||e)[d](i,a.md=function(n){e.hasAttribute("nochilddrag")&&l.elementFromPoint(n.pageX,n.pageY)!=a||(f=1,m=n.clientX,s=n.clientY,n.preventDefault())},0),o[d](r,a.mu=function(){f=0},0),o[d](c,a.mm=function(o){f&&((u=e.scroller||e).scrollLeft-=n=-m+(m=o.clientX),u.scrollTop-=t=-s+(s=o.clientY),e==l.body&&((u=l.documentElement).scrollLeft-=n,u.scrollTop-=t))},0)}(f[e++])};"complete"==l.readyState?u():o[d]("load",u,0),e.reset=u});
 
+const scrollDuration = 500;
+const nodesWithData = document.querySelectorAll('.node-with-data');
+
+// TODO: Make this sane.
+// https://gist.github.com/gre/1650294
 Math.easeInOutQuad = function (current_time, start_val, value_change, duration) {
-    current_time /= duration/2;
+    current_time /= duration / 2;
     if (current_time < 1) return value_change/2*current_time*current_time + start_val;
     current_time--;
     return -value_change/2 * (current_time*(current_time-2) - 1) + start_val;
@@ -26,29 +34,21 @@ function smoothScroll(element, to, duration) {
     animateScroll();
 }
 
-const nodesWithData = document.querySelectorAll('.node-with-data');
-
 function hideAllPopups() {
     document.querySelectorAll('.popup').forEach(p => {
         p.style.visibility = 'hidden';
     });
 }
 
-// // Hide all popups when any place outside the track is clicked
-// document.getElementById('timeline-wrapper').addEventListener('click', event => {
-//     hideAllPopups();
-// });
-
 // Each node, when clicked will...
 nodesWithData.forEach(node => {
     node.addEventListener('click', event => {
-
         // hide all other popups
         hideAllPopups();
 
         // will show its child popup...
         let nodePopup = node.getElementsByClassName('popup')[0];
-        nodePopup.style.visibility = 'visible';
+        nodePopup.style.visibility = 'visible';        
     });
 });
 
@@ -62,55 +62,61 @@ document.getElementById('behind').addEventListener('click', e => {
 });
 
 
-// instantiate the graph
-// view-source:https://shutterstock.github.io/rickshaw/examples/lines.html
-var seriesData = [ [], [], [] ];
-var random = new Rickshaw.Fixtures.RandomData(150);
+// Draw some random graphs. First, count all nodes
+const nodeCount = document.querySelectorAll('.node').length;
 
-for (var i = 0; i < 150; i++) {
-    random.addData(seriesData);
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var graph = new Rickshaw.Graph( {
-    element: document.getElementById('graph-wrapper'),
-    width: document.getElementById('timeline').getBoundingClientRect().width,
-    height: 250,
-    renderer: 'line',
-    series: [
-        {
-            color: "#c05020",
-            data: seriesData[0],
-            name: 'Flibbity'
-        }, {
-            color: "#30c020",
-            data: seriesData[1],
-            name: 'Flabbity'
-        }, {
-            color: "#6060c0",
-            data: seriesData[2],
-            name: 'Floo!'
-        }
-    ]
-} );
+function getRandomDataset(min, max) {
+    let data = [];
+    for (let i = nodeCount - 1; i >= 0; i--) {
+        data[i] = {
+            x: i,
+            y: getRandomIntInclusive(min, max)
+        };
+    }
+    return data;
+}
 
-graph.render();
+var rainGraph = new Rickshaw.Graph({
+    element: document.getElementById('rain'),
+    renderer: 'bar',
+    width: 2592,
+    height: 50,
+    series: [{
+        data: getRandomDataset(0, 3),
+        color: 'steelblue'
+    }]
+});
 
-var hoverDetail = new Rickshaw.Graph.HoverDetail( {
-    graph: graph
-} );
+rainGraph.render();
 
-var legend = new Rickshaw.Graph.Legend( {
-    graph: graph,
-    element: document.getElementById('legend')
+var randomGraphA = new Rickshaw.Graph({
+    element: document.getElementById('graph-a'),
+    renderer: 'area',
+    width: 2592,
+    height: 50,
+    series: [{
+        data: getRandomDataset(0, 12),
+        color: 'steelblue'
+    }]
+});
 
-} );
+randomGraphA.render();
 
-var shelving = new Rickshaw.Graph.Behavior.Series.Toggle( {
-    graph: graph,
-    legend: legend
-} );
+var randomGraphA = new Rickshaw.Graph({
+    element: document.getElementById('graph-b'),
+    renderer: 'lineplot',
+    width: 2592,
+    height: 50,
+    series: [{
+        data: getRandomDataset(4, 8),
+        color: 'steelblue'
+    }]
+});
 
-var axes = new Rickshaw.Graph.Axis.Time( {
-    graph: graph
-} );
-axes.render();
+randomGraphA.render();
